5aed82ea609c5bfff1d751e7ae57f1c5c04897bb
Salma2486 <138704540+Salma2486@users.noreply.github.com>
2024-06-06 13:50:56 +0000

    q

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Type-annotated function to_kv that takes a string k and an int OR float v"""
+from typing import List, Union, Tuple
+
+
+def to_kv(k:str, v:Union[int | float]) -> Tuple[str, float]:
+    """Returns a tuple with k and the square of v"""
+    return (k, v ** 2)

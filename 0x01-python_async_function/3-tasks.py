2dde82e68ab41797061a0b2349d7dbdf69519374
Salma2486 <138704540+Salma2486@users.noreply.github.com>
2024-06-10 10:58:00 +0000

    t

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""Tasks"""
+"""Tasks set gset gstr gh"""
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
-    """Task"""
+    """Task ear gset ygse"""
     return asyncio.create_task(wait_random(max_delay))

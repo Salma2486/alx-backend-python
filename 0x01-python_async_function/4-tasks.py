f9b8fd63d6e6ceadc385025a40979009ec639450
Salma2486 <138704540+Salma2486@users.noreply.github.com>
2024-06-10 11:03:35 +0000

    t

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Basic async syntax"""
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Return the list of all the delays"""
+    delays = [task_wait_random(max_delay) for i in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
